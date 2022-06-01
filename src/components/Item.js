89be149916606dd72a5add7123d62f874e40d490
Joseph Guillory <jagmanx99@yahoo.com>
2022-06-01 14:32:27 -0700

    First two deliverables

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { useState } from "react/cjs/react.production.min";
+// import ReactDOM from 'react-dom';
 
 function Item({ name, category }) {
 
-  // const [isAdded, setIsAdded] = useState(false);
+  const [isAdded, setIsAdded] = React.useState(false);
+  console.log(isAdded)
 
-  // const cart = isAdded ? "" : "in-cart" ;
-
-  // function clickToCart(){
-  //   setIsAdded((isAdded) => !isAdded);
-  // }
+  
+  function clickToCart(){
+    setIsAdded(function(isAdded){   
+      return !isAdded
+    })
+  }
+    
+  const cart = isAdded ? "in-cart" : "" ;
+  const addBtn = isAdded ? "remove" : "add" ;
+  const innerBtn = isAdded ? "Remove from Cart" : "Add to Cart" ;
 
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add" >Add to Cart</button>
+      <button className={addBtn} onClick={clickToCart}>{innerBtn}</button>
     </li>
   );
 }
