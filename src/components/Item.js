e02881e37973740c29afd74d3f76a63fbfa176c0
Joseph Guillory <jagmanx99@yahoo.com>
2022-05-31 23:57:47 -0700

    App.js working

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 
 function Item({ name, category }) {
+
+  // const [isAdded, setIsAdded] = useState(false);
+
+  // const cart = isAdded ? "" : "in-cart" ;
+
+  // function clickToCart(){
+  //   setIsAdded((isAdded) => !isAdded);
+  // }
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" >Add to Cart</button>
     </li>
   );
 }
